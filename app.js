42aa6146d2c98540909723f436c8a7e013061dd3
Usman Ali <38491660+2016CS107@users.noreply.github.com>
2020-07-31 17:28:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200, https://muhammadusmanali.codes"),
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200"),
     res.setHeader(
       "Access-Control-Allow-Headers",
       "*"
