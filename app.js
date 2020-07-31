360b5f80da75e6cefe9ae23b2700b0e56d29e785
Usman Ali <38491660+2016CS107@users.noreply.github.com>
2020-07-31 17:18:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"),
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "Origin, x-Requested-With, Content-Type, Accept"
+      "*"
     ),
     res.setHeader("Access-Control-Allow-Methods", " GET, OPTIONS");
   next();
